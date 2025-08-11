e4f09f6ec439a9d205c280dca185d83539bd1c54
Angelrmatoz <angelrmatoz@gmail.com>
2025-08-11 16:13:10 -0400

    feat: implement login functionality with JWT authentication and user validation

--- Backend/controllers/blogs.js
+++ Backend/controllers/blogs.js
@@ -1,15 +1,42 @@
 import Blog from '../models/blog.js';
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const blogsRouter = express.Router();
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization');
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '');
+    }
+    return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
     response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response) => { 
+blogsRouter.post('/', async (request, response) => {
     const { title, author, url, likes } = request.body;
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    
+    if (!decodedToken.id) {
+        return response.status(401).json({
+            error: 'token invalid'
+        })
+    }
+
+    if (!decodedToken) {
+        return response.status(401).json({ error: 'token missing or invalid' });
+    }
+
+    const user = await User.findById(decodedToken.id);
+
+    if (!user) {
+        return response.status(401).json({ error: 'token missing or invalid' });
+    }
 
     if (!title || !url) {
         return response.status(400).json({ error: 'title and url are required' });
--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,7 @@ import config from './utils/config.js';
 import blogsRouter from './controllers/blogs.js';
 import './mongo.js';
 import usersRouter from './controllers/users.js';
+import loginRouter from './controllers/login.js';
 
 const app = express(); 
 app.use(morgan('tiny'));
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
+app.use('/api/login', loginRouter);
 
 if (process.env.NODE_ENV !== 'test') {
     app.listen(config.PORT, () => {
